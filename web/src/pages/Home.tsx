7ab07b936690f114f86a694770013dc24ea2a3b8
claudiobzrr <claudio.bezerra1998@gmail.com>
2022-11-18 11:34:54 -0300

    change group of elements

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -9,14 +9,14 @@ export function Home(){
     return(
         <div className='w-[100%] h-[100vh] flex flex-col items-center justify-center'>
 
-            <div className='flex flex-col items-center justify-center'>
-                <img src={Logo} alt="Logo" className='w-44 absolute top-0'/>
-                <h1 className='font-bold text-2xl text-white mb-10 mt-20'>
-                    Entrar
-                </h1>
+            <div className='flex flex-row items-center justify-center fixed top-0'>
+                <img src={Logo} alt="Logo" className="w-44" />
             </div>
             <form className='max-w-screen-sm max-h-sm flex flex-col items-center justify-center'>
-                <div className='mt-5 pb-1 flex flex-row items-center justify-center border-darkRed border-solid border-b-[1px]'>
+                <h1 className='font-bold text-2xl text-white mb-5 mt-20'>
+                    Entrar
+                </h1>
+                <div className='mt-8 pb-1 flex flex-row items-center justify-center border-darkRed border-solid border-b-[1px]'>
                     <User
                         size={24}
                         className='mr-2 text-darkRed'
@@ -27,21 +27,23 @@ export function Home(){
                         type="text"
                     />
                 </div>
-                <div className='mt-5 pb-1 flex flex-row items-center justify-center border-darkRed border-solid border-b-[1px]'>
+                <div className='mt-8 pb-1 flex flex-row items-center justify-center border-darkRed border-solid border-b-[1px]'>
                     <Lock
                         size={28}
                         className='mr-2 text-darkRed '
                     />
                     <input
                         placeholder='Digite sua senha'
-                        className='bg-dark placeholder:text-darkRed text-darkRed text-center pr-[36px] focus:outline-none' 
+                        className='bg-dark text-darkRed placeholder:text-darkRed text-center pr-[36px] focus:outline-none' 
                         type="password" 
                     />
                 </div>
                 <Button 
-                    className='mt-8 rounded-full bg-darkRed w-[100%] p-1 text-white font-semibold'
+                    className='mt-8 rounded-full bg-darkRed w-[100%] p-1 text-white font-semibold text-lg'
                     title="Login"
                 />
+
+                <a  href='/home' className='text-white font-semibold text-sm mt-4'>Esqueci minha senha</a>
             </form>
         </div>
     )
